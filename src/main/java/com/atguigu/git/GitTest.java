6c13cde796e095f19b6660b15fef6e58ee1a7693
ong <onglipwei722@gmail.com>
2022-08-03 01:12:02 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.atguigu.git;
+
+/**
+ * @author onglipwei
+ * @create 2022-08-03 1:09 AM
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
